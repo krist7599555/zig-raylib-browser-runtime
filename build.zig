2e1a6512e72fdbe1085d58b31de00201eb0798f5
Krist Ponpairin <krist7599555@gmail.com>
2026-02-15 20:00:33 +0700

    feat: MVP build raylib to web

--- build.zig
+++ build.zig
@@ -1,59 +1,93 @@
 const std = @import("std");
+const rlz = @import("raylib_zig");
 
 pub fn build(b: *std.Build) void {
-    const optimize = b.standardOptimizeOption(.{});
+    // TARGET
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .wasm32,
+        .os_tag = .emscripten,
+    });
+    const optimize = b.standardOptimizeOption(.{
+        .preferred_optimize_mode = .ReleaseFast,
+    });
 
-    // OUTPUT: zig-out/lib/libgame.a
-    const lib = b.addLibrary(.{
-        .name = "game",
+    const raylib_mod = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib_raylib: *std.Build.Step.Compile = raylib_mod.artifact("raylib");
+    {
+        lib_raylib.root_module.addCMacro("PLATFORM_WEB", "1"); // fix raylib shader opengl -> webgl
+        lib_raylib.root_module.addCMacro("GRAPHICS_API_OPENGL_ES2", "1"); // fix raylib shader opengl -> webgl
+    }
+
+    const lib_main: *std.Build.Step.Compile = b.addLibrary(.{
+        .name = "main",
         .linkage = .static,
-
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-            .target = b.resolveTargetQuery(.{
-                .cpu_arch = .wasm32,
-                .os_tag = .emscripten,
-            }),
+            .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{
+                    // when @import("raylib") will look to raylib.h
+                    .name = "raylib",
+                    .module = raylib_mod.module("raylib"),
+                },
+            },
         }),
     });
-    b.installArtifact(lib);
 
-    // 2) emcc step
-    const export_func = .{
-        "_game_init",
-        "_game_update",
-        "_game_test_logging",
-        "_game_test_return_str",
-        "_game_test_a",
-        "_game_test_b",
-        "_game_test_c",
-        "_game_test_d",
-    };
+    // OUT: .js + .wasm FROM .a + raylib.a
+    const emcc_linker: *std.Build.Step.Run = b.addSystemCommand(&.{"emcc"});
+    {
+        emcc_linker.addArgs(&.{ "-o", "zig-out/lib/game.mjs" });
+        emcc_linker.addArgs(&.{ "-o", "zig-out/lib/game.js" });
+        emcc_linker.addFileArg(lib_main.getEmittedBin());
+        emcc_linker.addFileArg(lib_raylib.getEmittedBin());
+        emcc_linker.addArgs(&.{"--no-entry"}); // no `fn main()`
+        emcc_linker.addArgs(&.{ "-s", "STANDALONE_WASM=1" });
+        emcc_linker.addArgs(&.{ "-s", "USE_GLFW=3" });
+        emcc_linker.addArgs(&.{ "-s", "USE_WEBGL2=1" });
+        emcc_linker.addArgs(&.{ "-s", "FULL_ES2=1" });
+        emcc_linker.addArgs(&.{ "-s", "MAX_WEBGL_VERSION=2" });
+        emcc_linker.addArgs(&.{
+            "-s",
+            join_list("EXPORTED_FUNCTIONS", &.{
+                "_game_init",
+                "_game_update",
+                "_game_test_logging",
+                "_game_test_return_str",
+                "_game_test_a",
+                "_game_test_b",
+                "_game_test_c",
+                "_game_test_d",
+            }),
+        });
+    }
 
-    const export_str = comptime blk: {
-        var str: []const u8 = "";
-        for (export_func, 0..) |name, i| {
-            str = str ++ "'" ++ name ++ "'";
-            if (i < export_func.len - 1) str = str ++ ",";
-        }
-        break :blk "EXPORTED_FUNCTIONS=[" ++ str ++ "]";
-    };
-    const emcc = b.addSystemCommand(&.{
-        "emcc",
-        "zig-out/lib/libgame.a",
-        "-o",
-        "zig-out/lib/game.js",
-        "-s",
-        export_str,
-        "-s",
-        "STANDALONE_WASM=1",
-        "--no-entry",
-    });
+    // GRAPH
+    // .js+.wasm require libgame.a, require raylib.a
+    emcc_linker.step.dependOn(&lib_raylib.step);
+    emcc_linker.step.dependOn(&lib_main.step);
 
-    // 3) บอกว่า emcc ต้องรอ lib เสร็จก่อน
-    emcc.step.dependOn(&lib.step);
+    // WRITE (Optional)
+    b.installArtifact(lib_main); // lib/libapp.a
+    b.installArtifact(lib_raylib); // lib/libraylib.a
 
-    // 4) ผูกเข้ากับ `zig build`
-    b.getInstallStep().dependOn(&emcc.step);
+    // install = require .js+.wams
+    b.getInstallStep().dependOn(&emcc_linker.step);
+}
+
+pub fn join_list(
+    comptime key: []const u8,
+    comptime list: []const []const u8,
+) []const u8 {
+    comptime var str: []const u8 = "";
+    inline for (list, 0..) |name, i| {
+        str = str ++ "'" ++ name ++ "'";
+        if (i < list.len - 1) str = str ++ ",";
+    }
+    return key ++ "=[" ++ str ++ "]";
 }
--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,48 @@
 const std = @import("std");
+const rl = @import("raylib");
 
 extern fn emscripten_log(flags: i32, msg: [*:0]const u8, ...) void;
 extern fn emscripten_console_log(msg: [*:0]const u8) void;
 
-export fn game_init() void {}
+var game_title: [100:0]u8 = undefined;
+
+export fn game_init() void {
+
+    // rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    // ขนาดอะไรก็ได้ canvas จะ scale เอง
+    rl.initWindow(1024, 720, "raylib + zig + wasm");
+
+    // บนเว็บ SetTargetFPS ไม่มีผล แต่ไม่ใส่ก็โดนมองแรง
+    rl.setTargetFPS(60);
+
+    @memmove(game_title[0..11], "Game Title\x00");
+}
+
+var time: f32 = 0;
 export fn game_update(dt: f32) void {
-    _ = dt;
+    time += dt;
+    // _ = dt;
+    rl.beginDrawing();
+    defer rl.endDrawing();
+
+    rl.clearBackground(rl.Color.blue);
+
+    const w: i32 = rl.getScreenWidth();
+    const h: i32 = rl.getScreenHeight();
+
+    rl.drawText(
+        &game_title,
+        @mod(@as(i32, @intFromFloat(@ceil(time * 0.3))), w),
+        @divFloor(h, 2),
+        20,
+        rl.Color.ray_white,
+    );
+
+    var buf: [64:0]u8 = undefined;
+    const text = std.fmt.bufPrintZ(&buf, "dt = {d:.5}", .{dt}) catch "???";
+
+    rl.drawText(text, 10, 10, 20, .green);
+    rl.drawFPS(50, 50);
 }
 
 export fn game_test_logging() i32 {
